46feaefa99218ae886b6050b3f7e4d3c16bc199a
Jeff LADIRAY <ladirayjeff@gmail.com>
2024-02-26 17:09:33 +0100

    fix: fix

--- index.ts
+++ index.ts
@@ -10,38 +10,41 @@ import couponsCustomization from './customizations/coupons';
 import pg from 'pg';
 import type { Schema } from './typings';
 import path from 'path';
-console.log(__dirname);
-console.log(path.resolve(__dirname, './.forestadmin-schema.json'));
 
-const agent = createAgent<Schema>({
-  authSecret: process.env.FOREST_AUTH_SECRET!,
-  envSecret: process.env.FOREST_ENV_SECRET!,
-
-  isProduction: process.env.NODE_ENV === 'production',
-  
-  typingsPath: './typings.ts',
-  typingsMaxDepth: 5,
-});
-
-agent
-  .addDataSource(
-    createSqlDataSource({
-      uri: process.env.DATABASE_URL,
-      schema: process.env.DATABASE_SCHEMA,
-      sslMode: process.env.DATABASE_SSL_MODE as SslMode,
-      dialect: 'postgres',
-      dialectModule: pg,
-    }),
-  )
-  .customizeCollection('users', usersCustomization)
-  .customizeCollection('tickets', ticketsCustomization)
-  .customizeCollection('orders', ordersCustomization)
-  .customizeCollection('coupons', couponsCustomization);
-
-agent.mountOnStandaloneServer(Number(process.env.APPLICATION_PORT));
-
-agent.start().catch(error => {
-  // eslint-disable-next-line no-console
-  console.error('\x1b[31merror:\x1b[0m Forest Admin agent failed to start\n', error.stack);
-  process.exit(1);
-});
+export default async () => {
+  try {
+    console.log(__dirname);
+    console.log(path.resolve(__dirname, './.forestadmin-schema.json'));
+    
+    const agent = createAgent<Schema>({
+      authSecret: process.env.FOREST_AUTH_SECRET!,
+      envSecret: process.env.FOREST_ENV_SECRET!,
+    
+      isProduction: process.env.NODE_ENV === 'production',
+      
+      typingsPath: './typings.ts',
+      typingsMaxDepth: 5,
+    });
+    
+    agent
+      .addDataSource(
+        createSqlDataSource({
+          uri: process.env.DATABASE_URL,
+          schema: process.env.DATABASE_SCHEMA,
+          sslMode: process.env.DATABASE_SSL_MODE as SslMode,
+          dialect: 'postgres',
+          dialectModule: pg,
+        }),
+      )
+      .customizeCollection('users', usersCustomization)
+      .customizeCollection('tickets', ticketsCustomization)
+      .customizeCollection('orders', ordersCustomization)
+      .customizeCollection('coupons', couponsCustomization);
+    
+    agent.mountOnStandaloneServer(Number(process.env.APPLICATION_PORT));
+    
+    await agent.start();
+  } catch(err) {
+    console.error(err);
+  }
+}
